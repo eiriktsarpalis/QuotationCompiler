72ad897437686e4dc377ede83651741add61270e
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-07-08 01:02:11 +0300

    fix nuget package issue

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 0.0.6-alpha
+* Fix package issues.
+
 ### 0.0.5-alpha
 * Bugfix.
 
--- src/QuotationCompiler/AssemblyInfo.fs
+++ src/QuotationCompiler/AssemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyVersionAttribute("0.0.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.5"
+    let [<Literal>] Version = "0.0.6"
