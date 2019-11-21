92895924d5d7698bed21c3dac84cc57acabdec54
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-11-21 01:02:01 +0000

    add a benchmarks project

---
+++ tests/QuotationCompiler.Benchmarks/Common.fs
@@ -0,0 +1,29 @@
+﻿[<AutoOpen>]
+module QuotationCompiler.Benchmarks.Common
+
+open System.Reflection
+
+open FSharp.Quotations
+open FSharp.Quotations.Patterns
+open QuotationCompiler
+
+type Expr with
+    static member GetReflectedDefinition (e : Expr<'T>) =
+        let rec methodOf (e : Expr) =
+            match e with
+            | Lambda(_,b) -> methodOf b
+            | Call(_,m,_) -> m :> MethodBase
+            | NewObject(c,_) -> c :> MethodBase
+            | PropertyGet(_,p,_) -> p.GetGetMethod(true) :> MethodBase
+            | _ -> invalidArg "e" "should be method call."
+
+        let m = methodOf e 
+        let re = Expr.TryGetReflectedDefinition m |> Option.get
+        Expr.Cast<'T>(re)
+
+let compileAll (e : Expr<'T>) =
+    {|
+        powerpack = lazy(FSharp.Quotations.Evaluator.QuotationEvaluator.Evaluate e)
+        unquote = lazy(Swensen.Unquote.Operators.eval e)
+        qcompiler = lazy(QuotationCompiler.Eval e |> Async.RunSynchronously)
+    |}
---
+++ tests/QuotationCompiler.Benchmarks/SquareRoot.fs
@@ -0,0 +1,31 @@
+﻿module QuotationCompiler.Benchmarks.SquareRoot
+
+open FSharp.Quotations
+open BenchmarkDotNet.Attributes
+
+// Square root approximation algorithm taken from SICP
+
+[<ReflectedDefinition>]
+let sqrt (x : float) =
+    let epsilon = 1e-10
+    let rec approximate (y : float) =
+        let y' = (y + x / y) / 2.
+        if abs (y - y') < epsilon then y'
+        else
+            approximate y'
+
+    approximate x
+
+[<MemoryDiagnoser>]
+type SqrtBenchmark() =
+    let inputValue = 1821.
+    let compiledMethods = Expr.GetReflectedDefinition <@ sqrt @> |> compileAll
+    
+    [<Benchmark(Description = "Managed", Baseline = true)>]
+    member __.Managed() = sqrt inputValue |> ignore
+    [<Benchmark(Description = "Unquote")>]
+    member __.Unquote() = compiledMethods.unquote.Value inputValue |> ignore
+    [<Benchmark(Description = "FSharp.Quotations.Evaluator")>]
+    member __.Powerpack() = compiledMethods.powerpack.Value inputValue |> ignore
+    [<Benchmark(Description = "QuotationCompiler")>]
+    member __.QCompiler() = compiledMethods.qcompiler.Value inputValue |> ignore
